c6ca313508d0852875b9842ce023061fdbb78274
Bruno Henrique <brunoziycabr959@gmail.com>
2024-03-28 08:20:10 -0300

    segundo commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Sedans from "./assets/sedans.svg";
+import Suvs from "./assets/suvs.svg";
+import Luxury from "./assets/luxury.svg"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
+
+
+export default function App(){
+  return(
+  <main>
+    <section>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <img src={Sedans} alt="icone sedans"/>
+    <h2>Sedans</h2>
+    <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+    <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver Mais</a>
+    t
+    </div>
+
+    <div>
+      <img src={Suvs} alt="icone SUVS" />
+      <h2>SUVs</h2>
+      <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+      <a href="https://www.webmotors.com.br/suvs" target="_blank">Ver Mais</a>
+    </div>
+
+
+<div>
+<img src={Luxury} alt="icone luxo"/>
+    <h2>Luxury</h2>
+    <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+    <a href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver Mais</a>
+</div>
+
+
+
+    </section>
+    </main>
   )
 }
 
-export default App
+
+
