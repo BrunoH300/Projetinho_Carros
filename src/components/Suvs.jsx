6d8584b5bca44fac5c6c0fb9762289d2891e8e33
Bruno Henrique <brunoziycabr959@gmail.com>
2024-03-28 10:30:27 -0300

    criação de componetes

---
+++ src/components/Suvs.jsx
@@ -0,0 +1,13 @@
+import Suvs from "../assets/suvs.svg";
+export default function App(){
+    return(
+    
+    <div className="bg-green p-12">
+    <img src={Suvs} alt="icone SUVS" className="mb-7"/>
+    <h2 className="font-shoulders mb-7 uppercase 4xl font-bold text-offwhitebg">SUVs</h2>
+    <p className="font-lexend mb-7 text-offwhite lg:mb-20">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+    <a className= "font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-green border-2 border-offwhitebg hover:bg-green hover:text-offwhitebg" href="https://www.webmotors.com.br/suvs" target="_blank">Ver Mais</a>
+  </div>
+
+    )
+}
